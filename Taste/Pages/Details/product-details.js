c850ae57f50426067c0a53f5a68c73aaec712ffe
phuongth <phuongth@nichietsuvn.com>
2021-01-23 16:17:35 +0700

    Add plus mins button and its script

--- Taste/Pages/Details/product-details.js
+++ Taste/Pages/Details/product-details.js
@@ -1,21 +1,72 @@
-﻿(function ($) {
-	$('#thumbcarousel').carousel(0);
-	var $thumbItems = $('#thumbcarousel .item');
-	$('#carousel').on('slide.bs.carousel', function (event) {
-		var $slide = $(event.relatedTarget);
-		var thumbIndex = $slide.data('thumb');
-		var curThumbIndex = $thumbItems.index($thumbItems.filter('.active').get(0));
-		if (curThumbIndex > thumbIndex) {
-			$('#thumbcarousel').one('slid.bs.carousel', function (event) {
-				$('#thumbcarousel').carousel(thumbIndex);
-			});
-			if (curThumbIndex === ($thumbItems.length - 1)) {
-				$('#thumbcarousel').carousel('next');
-			} else {
-				$('#thumbcarousel').carousel(numThumbItems - 1);
-			}
-		} else {
-			$('#thumbcarousel').carousel(thumbIndex);
-		}
-	});
-})(jQuery);
+﻿
+$('.btn-number').click(function (e) {
+    e.preventDefault();
+
+    fieldName = $(this).attr('data-field');
+    type = $(this).attr('data-type');
+    var input = $("input[name='" + fieldName + "']");
+    var currentVal = parseInt(input.val());
+    if (!isNaN(currentVal)) {
+        if (type == 'minus') {
+
+            if (currentVal > input.attr('min')) {
+                input.val(currentVal - 1).change();
+            }
+            if (parseInt(input.val()) == input.attr('min')) {
+                $(this).attr('disabled', true);
+            }
+
+        } else if (type == 'plus') {
+
+            if (currentVal < input.attr('max')) {
+                input.val(currentVal + 1).change();
+            }
+            if (parseInt(input.val()) == input.attr('max')) {
+                $(this).attr('disabled', true);
+            }
+
+        }
+    } else {
+        input.val(0);
+    }
+});
+$('.input-number').focusin(function () {
+    $(this).data('oldValue', $(this).val());
+});
+$('.input-number').change(function () {
+
+    minValue = parseInt($(this).attr('min'));
+    maxValue = parseInt($(this).attr('max'));
+    valueCurrent = parseInt($(this).val());
+
+    name = $(this).attr('name');
+    if (valueCurrent >= minValue) {
+        $(".btn-number[data-type='minus'][data-field='" + name + "']").removeAttr('disabled')
+    } else {
+        alert('Sorry, the minimum value was reached');
+        $(this).val($(this).data('oldValue'));
+    }
+    if (valueCurrent <= maxValue) {
+        $(".btn-number[data-type='plus'][data-field='" + name + "']").removeAttr('disabled')
+    } else {
+        alert('Sorry, the maximum value was reached');
+        $(this).val($(this).data('oldValue'));
+    }
+
+
+});
+$(".input-number").keydown(function (e) {
+    // Allow: backspace, delete, tab, escape, enter and .
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+        // Allow: Ctrl+A
+        (e.keyCode == 65 && e.ctrlKey === true) ||
+        // Allow: home, end, left, right
+        (e.keyCode >= 35 && e.keyCode <= 39)) {
+        // let it happen, don't do anything
+        return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+    }
+});
