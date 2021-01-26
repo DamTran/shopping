46d09c6c7269985ab0ba247fe6539ec96a275800
Farsky <farsky_vt@yahoo.com>
2021-01-26 23:26:12 +0700

    Add TODO items

--- Taste/Pages/AboutUs/Index.cshtml.cs
+++ Taste/Pages/AboutUs/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿namespace Taste.Pages.AboutUs
 {
+    // TODO: Should we use contact-us only?
     public class IndexModel : ApplicationPageModel
     {
         public IndexModel() : base("About Us")
--- Taste/Pages/ContactUs/Index.cshtml.cs
+++ Taste/Pages/ContactUs/Index.cshtml.cs
@@ -1,6 +1,6 @@
 ﻿namespace Taste.Pages.ContactUs
 {
-    // https://livedemo00.template-help.com/wt_prod-10492/theme/contact-us.html
+    // TODO: https://livedemo00.template-help.com/wt_prod-10492/theme/contact-us.html
     public class IndexModel : ApplicationPageModel
     {
         public IndexModel() : base("Contact Us")
