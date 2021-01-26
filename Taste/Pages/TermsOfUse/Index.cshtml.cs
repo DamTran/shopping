bbc9ac0d09f4405184916ea4d38fe069fa2a7e6c
Farsky <farsky_vt@yahoo.com>
2021-01-26 23:25:58 +0700

    Initialize TermsOfUse page

---
+++ Taste/Pages/TermsOfUse/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿namespace Taste.Pages.TermsOfUse
+{
+    // TODO: https://livedemo00.template-help.com/wt_prod-10492/theme/terms-of-use.html
+    public class IndexModel : ApplicationPageModel
+    {
+        public IndexModel() : base("Terms of Use")
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
