f94c5a2109368902410ff978c257ca64bc099701
Farsky <farsky_vt@yahoo.com>
2021-01-27 08:07:24 +0700

    Initialize Blog page

---
+++ Taste/Pages/Blog/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿namespace Taste.Pages.Blog
+{
+    // TODO: https://livedemo00.template-help.com/wt_prod-10492/theme/blog-list.html
+    public class IndexModel : ApplicationPageModel
+    {
+        public override string Title => "Blog";
+
+        public IndexModel()
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
