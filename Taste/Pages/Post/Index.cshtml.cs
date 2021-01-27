2a7aea87ef0fbe5b9319d4a460b7dec9106bcf9f
Farsky <farsky_vt@yahoo.com>
2021-01-27 08:08:08 +0700

    Initialize Post page

---
+++ Taste/Pages/Post/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿namespace Taste.Pages.Post
+{
+    // TODO: https://livedemo00.template-help.com/wt_prod-10492/theme/blog-post.html
+    public class IndexModel : ApplicationPageModel
+    {
+        public string PostId { get; }
+
+        public void OnGet(string id, string title)
+        {
+        }
+    }
+}
