9c3b090efc1a2b8b4d34db291d7a33f805ac620c
Farsky <farsky_vt@yahoo.com>
2021-01-27 08:25:06 +0700

    Initialize Checkout page

---
+++ Taste/Pages/Checkout/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿namespace Taste.Pages.Checkout
+{
+    // TODO: https://livedemo00.template-help.com/wt_prod-10492/theme/checkout.html
+    public class IndexModel : ApplicationPageModel
+    {
+        public override string Title => "Checkout";
+
+        public IndexModel()
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
