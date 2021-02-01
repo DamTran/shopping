fa46e0c48eb29d69161e9e3814d29ed8ac198191
Farsky <farsky_vt@yahoo.com>
2021-02-01 09:19:48 +0700

    Clean up project Utitlity

--- Taste.Utility/SD.cs
+++ Taste.Utility/SD.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Taste.Utility
+﻿namespace Taste.Utility
 {
     public static class SD
     {
@@ -11,19 +7,17 @@ namespace Taste.Utility
         public const string KitchenRole = "Kitchen";
         public const string CustomerRole = "Customer";
 
-
         public const string ShoppingCart = "ShoppingCart";
 
         public const string StatusSubmitted = "Submitted";
         public const string StatusInProcess = "Being Prepared";
         public const string StatusReady = "Ready for Pickup";
-        public const string StatusCompleted= "Completed";
+        public const string StatusCompleted = "Completed";
         public const string StatusCancelled = "Cancelled";
         public const string StatusRefunded = "Refunded";
 
         public const string PaymentStatusPending = "Pending";
         public const string PaymentStatusApproved = "Approved";
         public const string PaymentStatusRejected = "Rejected";
-
     }
 }
--- Taste.Utility/StripeSettings.cs
+++ Taste.Utility/StripeSettings.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Taste.Utility
+﻿namespace Taste.Utility
 {
     public class StripeSettings
     {
