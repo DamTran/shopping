63549de685e68ed192e15625ba494b980b75c76a
Farsky <farsky_vt@yahoo.com>
2021-02-15 23:59:03 +0700

    New API for easy debugging

--- Taste/Controllers/QueryController.cs
+++ Taste/Controllers/QueryController.cs
@@ -3,6 +3,8 @@
     using Dapper;
     using Microsoft.AspNetCore.Mvc;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using Taste.DataAccess.DbConnectionProvider;
 
     [Route("api")]
@@ -16,20 +18,65 @@
             _dbConnectionProvider = dbConnectionProvider;
         }
 
-        [HttpPost("{queryName}")]
-        public IActionResult Get(string queryName, [FromBody] object data)
+        [HttpGet("{queryName}")]
+        public IActionResult Get(string queryName)
         {
-            if (!_dbConnectionProvider.Queries.ContainsKey(queryName))
+            var selectedQuery = _dbConnectionProvider.Queries
+                .FirstOrDefault(kvp => string.Equals(kvp.Key, queryName, StringComparison.OrdinalIgnoreCase));
+            if (selectedQuery.Equals(default(KeyValuePair<string, string>)))
             {
                 var exception = new Exception("Invalid API call.");
                 return BadRequest(exception);
             }
 
-            string query = _dbConnectionProvider.Queries[queryName];
+            string query = selectedQuery.Value;
             using (var connection = _dbConnectionProvider.GetRemoteDbConnection())
             {
-                var result = connection.Query(query, data);
-                return Json(result);
+                try
+                {
+                    var result = connection.Query(query);
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return Problem(
+                        detail: ex.StackTrace
+                        , statusCode: 500
+                        , title: ex.Message
+                        , type: ex.GetType().FullName
+                        );
+                }
+            }
+        }
+
+        [HttpPost("{queryName}")]
+        public IActionResult Post(string queryName, [FromBody] object data)
+        {
+            var selectedQuery = _dbConnectionProvider.Queries
+                .FirstOrDefault(kvp => string.Equals(kvp.Key, queryName, StringComparison.OrdinalIgnoreCase));
+            if (selectedQuery.Equals(default(KeyValuePair<string, string>)))
+            {
+                var exception = new Exception("Invalid API call.");
+                return BadRequest(exception);
+            }
+
+            string query = selectedQuery.Value;
+            using (var connection = _dbConnectionProvider.GetRemoteDbConnection())
+            {
+                try
+                {
+                    var result = connection.Query(query, data);
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return Problem(
+                        detail: ex.StackTrace
+                        , statusCode: 500
+                        , title: ex.Message
+                        , type: ex.GetType().FullName
+                        );
+                }
             }
         }
     }
