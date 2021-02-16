fb18a8265789e8eb4c38b1d9ab8015b97265fa69
Farsky <farsky_vt@yahoo.com>
2021-02-16 08:50:05 +0700

    SEO-friendly URLs for products

---
+++ Taste/Pages/ProductDetails/Index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿namespace Taste.Pages.ProductDetails
+{
+    public class IndexModel : ApplicationPageModel
+    {
+        public IndexModel() : base("Product Details")
+        {
+        }
+
+        public void OnGet(int productId, string productLabel)
+        {
+        }
+    }
+}
