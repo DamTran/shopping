164b567e1f54301c286b89a11a3473514ddbfe6f
Farsky <farsky_vt@yahoo.com>
2021-02-16 14:57:50 +0700

    Updates for header

---
+++ Taste/Pages/SignIn/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿namespace Taste.Pages.SignIn
+{
+    public class IndexModel : ApplicationPageModel
+    {
+        public override string Title => "Login";
+
+        public IndexModel()
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
