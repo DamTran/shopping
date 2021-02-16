beb5ae74a986408f65bebfefe922dd76a2d8cd63
Farsky <farsky_vt@yahoo.com>
2021-02-16 16:30:29 +0700

    Register page

---
+++ Taste/Pages/Register/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿namespace Taste.Pages.Register
+{
+    public class IndexModel : ApplicationPageModel
+    {
+        public override string Title => "Register";
+
+        public IndexModel()
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
