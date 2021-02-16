44e098b41e7a79d94fa616eb2f0cc0f760f940c5
Farsky <farsky_vt@yahoo.com>
2021-02-16 16:22:00 +0700

    ForgotPassword page

---
+++ Taste/Pages/ForgotPassword/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿namespace Taste.Pages.ForgotPassword
+{
+    public class IndexModel : ApplicationPageModel
+    {
+        public override string Title => "Forgot Password";
+
+        public IndexModel()
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
