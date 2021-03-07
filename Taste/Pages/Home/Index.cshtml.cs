e2df9eff385f59958847c3f5f81609a18957adf4
Farsky <farsky_vt@yahoo.com>
2021-03-07 11:23:09 +0700

    Clean up Home page logic

--- Taste/Pages/Home/Index.cshtml.cs
+++ Taste/Pages/Home/Index.cshtml.cs
@@ -1,11 +1,8 @@
 ﻿namespace Taste.Pages.Home
 {
-    using Dapper;
     using Microsoft.AspNetCore.Http;
-    using System.Collections.Generic;
     using System.Security.Claims;
     using Taste.DataAccess.DbConnectionProvider;
-    using Taste.Models;
     using Taste.Utility;
 
     public class IndexModel : ApplicationPageModel
@@ -18,12 +15,10 @@
         }
 
         public override string Title => "Home";
-        public IEnumerable<MenuItem> MenuItemList { get; set; }
-        public IEnumerable<Category> CategoryList { get; set; }
 
         public void OnGet()
         {
-            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
             var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
             using (var connection = _dbConnectionProvider.GetDbConnection())
@@ -32,21 +27,8 @@
 
                 if (claim != null)
                 {
-                    // TODO: Check logic for getting cart item count
-                    string getCartItemCountQuery = _dbConnectionProvider.Queries["GetCartItemCount"];
-                    //int cartItemCount = connection.ExecuteScalar<int>(getCartItemCountQuery, new
-                    //{
-                    //    ApplicationUserId = claim.Value,
-                    //});
-                    //HttpContext.Session.SetInt32(SD.ShoppingCart, cartItemCount);
                     HttpContext.Session.SetInt32(SD.ShoppingCart, 0);
                 }
-
-                string listMenuItemQuery = _dbConnectionProvider.Queries["ListMenuItem"];
-                MenuItemList = connection.Query<MenuItem>(listMenuItemQuery);
-
-                string listCategoryQuery = _dbConnectionProvider.Queries["ListCategory"];
-                CategoryList = connection.Query<Category>(listCategoryQuery);
             }
         }
     }
